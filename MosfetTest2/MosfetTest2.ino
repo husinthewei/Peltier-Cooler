5c5106516df25747ec947e70262f2c67879208be
Weiwei Hu <wae3wae@yahoo.com>
2016-06-23 12:44:27 -0400

    Added diode temperature reading

--- MosfetTest2/MosfetTest2.ino
+++ MosfetTest2/MosfetTest2.ino
@@ -4,16 +4,28 @@ double psuV = 8;
 void setup() {
   pinMode(peltPin, OUTPUT);
   pinMode(peltPin1, OUTPUT);
+  Serial.begin(9600);
+  analogReference(INTERNAL);
+  pinMode(A0, INPUT_PULLUP);
+  pinMode(A1, OUTPUT);
+  digitalWrite(A1, LOW);
 }
 
 void loop() {
-  OutputVoltage(6,psuV,peltPin); //test to see whether mosfet actually outputs certain voltage
-  OutputVoltage(8,psuV,peltPin1); 
+  OutputVoltage(5, psuV, peltPin); //test to see whether mosfet actually outputs certain voltage
+  OutputVoltage(5, psuV, peltPin1);
   delay(100);
+
+
+  float in = analogRead(A0);
+  float C = (598 - in) / 2.43 + 27.5;
+  Serial.print(in);
+  Serial.print("       ");
+  Serial.println(C);
 }
 
-void OutputVoltage(double v, double SrcV, int pin){
-  analogWrite(pin, (v/SrcV) * 255);
+void OutputVoltage(double v, double SrcV, int pin) {
+  analogWrite(pin, (v / SrcV) * 255);
 }
 
 
--- diodeTempTest/diodeTempTest.ino
+++ diodeTempTest/diodeTempTest.ino
@@ -1,23 +1,20 @@
 //useful link: https://drive.google.com/file/d/0B8NyMDTl1ovXYndyVnZrdDhZd1U/edit
 //int diodePin = 0;
-
-double base = 1013;void setup() {
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  analogReference(INTERNAL);   
   pinMode(A0,INPUT_PULLUP);
   pinMode(A1,OUTPUT);
   digitalWrite(A1,LOW);
-  Serial.begin(9600);
 }
 
 void loop() {
- double in = analogRead(A0);
- Serial.print(in);
- Serial.print("    ");
- double V = (((1013 - in)/1023) * 5);
- double C = 27 + (V/0.02);
- Serial.print(C);
- Serial.print("    ");
- double F = (1.8)*C + 32;
- Serial.println(F);
- 
- delay(100);
+  // put your main code here, to run repeatedly:
+delay(100);
+float in = analogRead(A0);
+float C = (598 - in) / 2.43 + 27.5;
+Serial.print(in);
+Serial.print("       ");
+Serial.println(C);
 }
