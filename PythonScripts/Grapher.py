084080b1791c7ec7b262032969a8bb70031abd3f
Weiwei Hu <wae3wae@yahoo.com>
2016-07-28 13:33:07 -0400

    Inverted graph colors

--- PythonScripts/Grapher.py
+++ PythonScripts/Grapher.py
@@ -10,13 +10,16 @@ class Grapher:
     #Once the deque's are filled, they start replacing the oldest elements
     #Therefore, runs for more than 24 hours and only shows last 24 hours.
     def __init__(self, ProgramStartTime = time.strftime("%Y%m%dT%H%M%S")):
+        pg.setConfigOption('background', 'w')
+        pg.setConfigOption('foreground', 'k')
         self.xData = collections.deque(maxlen=8640) 
         self.yData = collections.deque(maxlen=8640) 
         self.Program_Start_Time = ProgramStartTime  
         self.app = QtGui.QApplication([])
         self.p = pg.plot()
-        self.curve = self.p.plot(pen=pg.mkPen('b'))      
+        self.curve = self.p.plot(pen=pg.mkPen('r', width=3))      
         self.initializeGraph() 
+
         
     #Setting how the plot looks
     def initializeGraph(self):
